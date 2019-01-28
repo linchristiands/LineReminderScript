aebcd79262a03a48cf9d3d361f8bdfe1088592b1
Christian Lin <linchristiands@gmail.com>
2019-01-28 19:40:28 +0900

    aaa

--- remindbot.js
+++ remindbot.js
@@ -36,16 +36,12 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const msg = { type: 'text', text: "毒を飲んでください!" };
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  return client.replyMessage(event.replyToken, msg);
 }
 
 // listen on port
